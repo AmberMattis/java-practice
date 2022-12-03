d693afc152963fdf1f0a08f42460ef15de815d1b
Amber Mattis <amber.mattis2@gmail.com>
2022-12-03 09:46:37 -0600

    Daily java practice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -774,9 +774,68 @@ public class Main {
         return count;
     }
 
+    /**
+     * Given an array of ints, return the number of 9's in the array.
+     */
+
+    //App Solution
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] == 9) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //My version of solution
+    public int arrayCount9MyLogic(int[] nums) {
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 9){
+                count++;
+            }
+        }
+
+        return count;
+    }
 
 
+    /**
+     *
+     * Given an array of ints, return true
+     * if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+     */
 
+    //App Solution
+    public boolean arrayFront9(int[] nums) {
+        // First figure the end for the loop
+        int end = nums.length;
+        if (end > 4) end = 4;
+
+        for (int i=0; i<end; i++) {
+            if (nums[i] == 9) return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Given an array of ints, return true if
+     * the sequence of numbers 1, 2, 3 appears in the array somewhere.
+     */
+
+    //App Solution
+    public boolean array123(int[] nums) {
+        // Note: iterate < length-2, so can use i+1 and i+2 in the loop
+        for (int i=0; i < (nums.length-2); i++) {
+            if (nums[i]==1 && nums[i+1]==2 && nums[i+2]==3) return true;
+        }
+        return false;
+    }
 
 
 
